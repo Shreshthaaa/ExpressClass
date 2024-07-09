94c67795a2a9c914613b563b1922b981c860c1f2
TicTacToe_sst <paliwalshreshtha@gmail.com>
2024-07-09 23:12:44 +0530

    get, post, put

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+
+app.use(express.json());
+app.use(middleWare);
+app.use(logger);
 let courses = [ 
     { id: 1, name: "java" }, 
     { id: 2, name: "javascript" }, 
@@ -10,6 +14,48 @@ let courses = [
 app.get('/courses', (req, res) => {
     res.json(courses);
 });
+
+app.post('/courses', (req, res) => {
+    console.log(req.body);
+    let singleCourse = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(singleCourse);
+    res.send(courses);
+});
+
+//put val update id 1 to spring
+// delete call delete id 2
+
+app.put('/courses/:id', (req, res) => {
+    try {
+        let singleCourse = courses.find((course) => {
+            return course.id === +req.params.id
+        })
+
+        if(!singleCourse) {
+            res.status(400).send('course does not exist');
+        }
+
+        singleCourse.name = req.body.name;
+        res.send(courses);
+    } catch(err) {
+        res.status(500).send(err);
+    }
+})
+
+function middleWare(req, res, next) {
+    console.log("called");
+    next();
+}
+
+//logger
+function logger(req, res, next) {
+    console.log(req.method, req.ip, req.hostname, new Date());
+    next();
+}
+
 
 app.listen(3000, () => {
     console.log("server started");
